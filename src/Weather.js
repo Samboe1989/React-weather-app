584a7f05b52361dd0fe3b71e45b712c8eb89f8cb
Samboe1989 <91028218+Samboe1989@users.noreply.github.com>
2022-03-17 17:17:49 +0100

    Added github link

--- src/Weather.js
+++ src/Weather.js
@@ -37,128 +37,154 @@ export default function Weather(props) {
 
   if (loaded) {
     return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit} className="mb-3 navigation">
+      <div>
+        <div className="Weather">
+          <form onSubmit={handleSubmit} className="mb-3 navigation">
+            <div className="row">
+              <div className="col-9">
+                <input
+                  onChange={updateCity}
+                  type="search"
+                  placeholder="Type a city.."
+                  autoComplete="off"
+                  className="city-input"
+                />
+              </div>
+              <div className="col-3">
+                <button type="submit" className="seach-button">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
           <div className="row">
-            <div className="col-9">
-              <input
-                onChange={updateCity}
-                type="search"
-                placeholder="Type a city.."
-                autoComplete="off"
-                className="city-input"
-              />
+            <div className="col-sm-7">
+              <div className="overview">
+                <h1>{city}</h1>
+                <ul>
+                  <li>Last updated: Friday 19:32</li>
+                </ul>
+              </div>
             </div>
-            <div className="col-3">
-              <button type="submit" className="seach-button">
-                Search
-              </button>
-            </div>
-          </div>
-        </form>
-        <div className="row">
-          <div className="col-sm-7">
-            <div className="overview">
-              <h1>{city}</h1>
-              <ul>
-                <li>Last updated: Friday 19:32</li>
-              </ul>
+            <div className="col-sm-5">
+              <div className="actual-temperature">
+                <strong>{Math.round(weather.temperature)}</strong>
+                <span className="units">
+                  <a href="/">°C</a>|<a href="/">°F</a>
+                </span>
+              </div>
             </div>
           </div>
-          <div className="col-sm-5">
-            <div className="actual-temperature">
-              <strong>{Math.round(weather.temperature)}</strong>
-              <span className="units">
-                <a href="/">°C</a>|<a href="/">°F</a>
-              </span>
+          <div className="row">
+            <div className="col-sm-7">
+              <div className="weather-description">
+                <img
+                  src={weather.icon}
+                  alt={weather.description}
+                  className="weather-img"
+                />
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-7">
-            <div className="weather-description">
-              <img
-                src={weather.icon}
-                alt={weather.description}
-                className="weather-img"
-              />
-            </div>
-          </div>
-          <div className="col-sm-5">
-            <div className="wind-humidity">
-              <h2>{weather.description}</h2>
-              <ul>
-                <li>Humidity:{weather.humidity}%</li>
-                <li>Wind: {Math.round(weather.wind)} km/h</li>
-                <li>Min temp:{Math.round(weather.tempMin)}°C</li>
-                <li>Max temp:{Math.round(weather.tempMax)}°C</li>
-              </ul>
+            <div className="col-sm-5">
+              <div className="wind-humidity">
+                <h2>{weather.description}</h2>
+                <ul>
+                  <li>Humidity:{weather.humidity}%</li>
+                  <li>Wind: {Math.round(weather.wind)} km/h</li>
+                  <li>Min temp:{Math.round(weather.tempMin)}°C</li>
+                  <li>Max temp:{Math.round(weather.tempMax)}°C</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
+        <footer>
+          This project was coded by Samantha Boersma and is
+          <a
+            class="bottomlink"
+            href="https://github.com/Samboe1989/SheCodes-responsive-final-project"
+            target="_blank"
+            title="Github page"
+          >
+            {""} open-Sourced on GitHub
+          </a>
+        </footer>
       </div>
     );
   } else {
     return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit} className="mb-3 navigation">
+      <div>
+        <div className="Weather">
+          <form onSubmit={handleSubmit} className="mb-3 navigation">
+            <div className="row">
+              <div className="col-sm-9">
+                <input
+                  onChange={updateCity}
+                  type="search"
+                  placeholder="Type a city.."
+                  autoComplete="off"
+                  className="city-input"
+                />
+              </div>
+              <div className="col-sm-3">
+                <button type="submit" className="seach-button">
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
           <div className="row">
-            <div className="col-sm-9">
-              <input
-                onChange={updateCity}
-                type="search"
-                placeholder="Type a city.."
-                autoComplete="off"
-                className="city-input"
-              />
+            <div className="col-sm-7">
+              <div className="overview">
+                <h1>Barcelona</h1>
+                <ul>
+                  <li>Last updated: Friday 19:32</li>
+                </ul>
+              </div>
             </div>
-            <div className="col-sm-3">
-              <button type="submit" className="seach-button">
-                Search
-              </button>
-            </div>
-          </div>
-        </form>
-        <div className="row">
-          <div className="col-sm-7">
-            <div className="overview">
-              <h1>Barcelona</h1>
-              <ul>
-                <li>Last updated: Friday 19:32</li>
-              </ul>
+            <div className="col-sm-5">
+              <div className="actual-temperature">
+                <strong>16</strong>
+                <span className="units">
+                  <a href="/">°C</a>|<a href="/">°F</a>
+                </span>
+              </div>
             </div>
           </div>
-          <div className="col-sm-5">
-            <div className="actual-temperature">
-              <strong>16</strong>
-              <span className="units">
-                <a href="/">°C</a>|<a href="/">°F</a>
-              </span>
+          <div className="row">
+            <div className="col-sm-7">
+              <div className="weather-description">
+                <img
+                  src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/028/128/original/cloud.png?1645860303"
+                  alt="Cloudy"
+                  className="weather-img"
+                />
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-sm-7">
-            <div className="weather-description">
-              <img
-                src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/028/128/original/cloud.png?1645860303"
-                alt="Cloudy"
-                className="weather-img"
-              />
-            </div>
-          </div>
-          <div className="col-sm-5">
-            <div className="wind-humidity">
-              <h2>Cloudy</h2>
-              <ul>
-                <li>Humidity: 90%%</li>
-                <li>Wind: 7km/h</li>
-                <li>Min temp:9°C</li>
-                <li>Max temp:11°C</li>
-              </ul>
+            <div className="col-sm-5">
+              <div className="wind-humidity">
+                <h2>Cloudy</h2>
+                <ul>
+                  <li>Humidity: 90%%</li>
+                  <li>Wind: 7km/h</li>
+                  <li>Min temp:9°C</li>
+                  <li>Max temp:11°C</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
+        <footer>
+          This project was coded by Samantha Boersma and is
+          <a
+            class="bottomlink"
+            href="https://github.com/Samboe1989/SheCodes-responsive-final-project"
+            target="_blank"
+            title="Github page"
+          >
+            {""} open-Sourced on GitHub
+          </a>
+        </footer>
       </div>
     );
   }
