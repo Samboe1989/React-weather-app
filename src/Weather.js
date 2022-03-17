6a37f199fcd7f89ba20b65ee811981b02a4a9770
Samboe1989 <91028218+Samboe1989@users.noreply.github.com>
2022-03-17 16:25:57 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -31,69 +31,8 @@ export default function Weather(props) {
     setCity(event.target.value);
   }
 
-  let form = (
-    <div className="Weather">
-      <form onSubmit={handleSubmit} className="mb-3 navigation">
-        <div className="row">
-          <div className="col-9">
-            <input
-              onChange={updateCity}
-              type="search"
-              placeholder="Type a city.."
-              autoComplete="off"
-              className="city-input"
-            />
-          </div>
-          <div className="col-3">
-            <button type="submit" className="seach-button">
-              Search
-            </button>
-          </div>
-        </div>
-      </form>
-      <div className="row">
-        <div className="col-7">
-          <div className="overview">
-            <h1>{props.city}</h1>
-            <ul>
-              <li>Last updated: Friday 19:32</li>
-            </ul>
-          </div>
-        </div>
-        <div className="col-5">
-          <div className="actual-temperature">
-            <strong>{weather.temperature} °C </strong>
-            <span className="units">
-              <a href="/">°C</a>|<a href="/">°F</a>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-7">
-          <div className="weather-description">
-            <img
-              src={weather.icon}
-              alt={weather.description}
-              className="weather-img"
-            />
-          </div>
-        </div>
-        <div className="col-5">
-          <div className="wind-humidity">
-            <h2>{weather.description}</h2>
-            <ul>
-              <li>{weather.humidity}%</li>
-              <li>Wind: {weather.wind} km/h</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-
-  return (
-    <div>
+  if (loaded) {
+    return (
       <div className="Weather">
         <form onSubmit={handleSubmit} className="mb-3 navigation">
           <div className="row">
@@ -146,12 +85,73 @@ export default function Weather(props) {
               <h2>{weather.description}</h2>
               <ul>
                 <li>Humidity:{weather.humidity}%</li>
-                <li>Wind: {weather.wind} km/h</li>
+                <li>Wind: {Math.round(weather.wind)} km/h</li>
               </ul>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit} className="mb-3 navigation">
+          <div className="row">
+            <div className="col-9">
+              <input
+                onChange={updateCity}
+                type="search"
+                placeholder="Type a city.."
+                autoComplete="off"
+                className="city-input"
+              />
+            </div>
+            <div className="col-3">
+              <button type="submit" className="seach-button">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="row">
+          <div className="col-7">
+            <div className="overview">
+              <h1>Barcelona</h1>
+              <ul>
+                <li>Last updated: Friday 19:32</li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="actual-temperature">
+              <strong>16</strong>
+              <span className="units">
+                <a href="/">°C</a>|<a href="/">°F</a>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-7">
+            <div className="weather-description">
+              <img
+                src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/028/128/original/cloud.png?1645860303"
+                alt="Cloudy"
+                className="weather-img"
+              />
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="wind-humidity">
+              <h2>Cloudy</h2>
+              <ul>
+                <li>Humidity: 90%{weather.humidity}%</li>
+                <li>Wind: 5km/h</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
