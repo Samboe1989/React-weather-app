f857d685385550f435c28ebb43744d6729f0c486
Samboe1989 <91028218+Samboe1989@users.noreply.github.com>
2022-03-29 10:36:11 +0200

    CSS style changes

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,24 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  let apiKey = "6d9d93b7d32e34850e611e89547fc660";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thursday</div>
-          <WeatherIcon code="01d" />
+          <WeatherIcon code="01d" className="WeatherForecast-icon" />
           <div className="WeatherForecast-temperatures">
             <div className="WeatherForecast-temperature-max">Min 19°</div>
             <div className="WeatherForecast-temperature-min">Max 10°</div>
